b11a248a3df9c3fcf394e1122515864a328d88df
BenardBett <kiplangatbett5@gmail.com>
2021-01-29 22:40:10 +0300

    created a two classes for post and profile in form.py file

--- myneighbour/forms.py
+++ myneighbour/forms.py
@@ -8,3 +8,13 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model= User
         fields= ['username','email','password1', 'password2']
+        
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['username']
+        
+class PostForm(forms.ModelForm):
+    class Meta:
+        model=Post
+        exclude=['username','neighbourhood','avatar']
