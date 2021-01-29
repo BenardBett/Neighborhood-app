0e3bb179f05a176925bdd91a575c57d4a76c860f
BenardBett <kiplangatbett5@gmail.com>
2021-01-29 22:43:43 +0300

    regoistered my models in admin file

--- myneighbour/admin.py
+++ myneighbour/admin.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Authorities)
+admin.site.register(Health)
+admin.site.register(Post)
