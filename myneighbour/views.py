e250041ae05a6e21079d31644f64b04cfd20e637
BenardBett <kiplangatbett5@gmail.com>
2021-01-29 22:14:15 +0300

    defined a method for registration

--- myneighbour/views.py
+++ myneighbour/views.py
@@ -13,4 +13,18 @@ def index(request):
     except ObjectDoesNotExist:
         return redirect('create-profile')
 
-    return render(request,'index.html'
+    return render(request,'index.html')
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'registration/register.html', context)
+
