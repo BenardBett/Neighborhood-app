17b8758dd85370da6104a3a30598c7a1655d44e2
BenardBett <kiplangatbett5@gmail.com>
2021-01-30 11:16:48 +0300

    import forms and models in views.py file

--- myneighbour/views.py
+++ myneighbour/views.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
